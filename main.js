c8ee9514dd102f84e3e7e372ee4f8cf59a2e8b99
evanvada <evanvadapro@gmail.com>
2023-07-27 22:58:36 +0200

    responsive bottom hero & call to action done

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ window.addEventListener('scroll', function() {
 	let bg1_scroll_percent = (window.scrollY-hero.offsetTop)/hero.offsetHeight || 0
 	bg1.style.backgroundPosition = "50% calc(50% + " + bg1_scroll_percent*200 + "px)"
 
-	// let bg2_scroll_percent = (window.scrollY-hero_bottom.offsetTop)/hero_bottom.offsetHeight || 0
-	// bg2.style.backgroundPosition = "50% calc(50% + " + bg2_scroll_percent*200 + "px)"
+	let bg2_scroll_percent = (window.scrollY-hero_bottom.offsetTop)/hero_bottom.offsetHeight || 0
+	bg2.style.backgroundPosition = "50% calc(50% + " + bg2_scroll_percent*200 + "px)"
 });
 
 
