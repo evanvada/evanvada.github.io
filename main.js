58b3526fe59a7420013d2f65d8d5fbec6025d6ce
evanvada <evanvadapro@gmail.com>
2023-08-07 20:26:32 +0200

    added my portfolio's case study page

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ let bg2 = document.querySelector('.bottom-hero__bg')
 
 
 window.addEventListener('scroll', function() {
-	let bg1_scroll_percent = (window.scrollY-hero.offsetTop)/hero.offsetHeight || 0
-	bg1.style.backgroundPosition = "50% calc(50% + " + bg1_scroll_percent*200 + "px)"
+	// let bg1_scroll_percent = (window.scrollY-hero.offsetTop)/hero.offsetHeight || 0
+	// bg1.style.backgroundPosition = "50% calc(50% + " + bg1_scroll_percent*200 + "px)"
 
-	let bg2_scroll_percent = (window.scrollY-hero_bottom.offsetTop)/hero_bottom.offsetHeight || 0
-	bg2.style.backgroundPosition = "50% calc(50% + " + bg2_scroll_percent*200 + "px)"
+	// let bg2_scroll_percent = (window.scrollY-hero_bottom.offsetTop)/hero_bottom.offsetHeight || 0
+	// bg2.style.backgroundPosition = "50% calc(50% + " + bg2_scroll_percent*200 + "px)"
 });
 
 
